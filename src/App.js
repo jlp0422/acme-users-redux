b00fa1f637ce985277c8caef82fd9d8237a08bbb
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-03-17 11:31:51 -0400

    initial structure and skeleton set up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import React from 'react';
+import { Link, HashRouter as Router, Route } from 'react-router-dom';
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>These are Acme Users and Products</h1>
+      </div>
+    )
+  }
+}
